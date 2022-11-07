ac3aa30b708d64651463f5f53e5e54cddc46bd51
Tom Irimescu <tom@irimescu.com>
2022-11-07 16:25:56 -0500

    Add product & add review mutations

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -13,4 +13,39 @@ exports.Mutation = {
 
     return newCategory;
   },
+
+  addProduct: (parent, { input }, { products }) => {
+    const { name, image, price, onSale, quantity, categoryId } = input;
+
+    const newProduct = {
+      id: uuid(),
+      name,
+      image,
+      price,
+      onSale,
+      quantity,
+      categoryId,
+    };
+
+    products.push(newProduct);
+
+    return newProduct;
+  },
+
+  addReview: (parent, { input }, { reviews }) => {
+    const { date, title, comment, rating, productId } = input;
+
+    const newReview = {
+      id: uuid(),
+      date,
+      title,
+      comment,
+      rating,
+      productId,
+    };
+
+    reviews.push(newReview);
+
+    return newReview;
+  },
 };
