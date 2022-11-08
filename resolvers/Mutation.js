be2034e92f5216177f3827156a5f759018a3f169
Tom Irimescu <tom@irimescu.com>
2022-11-08 11:24:59 -0500

    Add delete category|product|review, update context

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,7 +1,7 @@
 const { v4: uuid } = require("uuid");
 
 exports.Mutation = {
-  addCategory: (parent, { input }, { categories }) => {
+  addCategory: (parent, { input }, { db }) => {
     const { name } = input;
 
     const newCategory = {
@@ -9,12 +9,12 @@ exports.Mutation = {
       name,
     };
 
-    categories.push(newCategory);
+    db.categories.push(newCategory);
 
     return newCategory;
   },
 
-  addProduct: (parent, { input }, { products }) => {
+  addProduct: (parent, { input }, { db }) => {
     const { name, image, price, onSale, quantity, categoryId } = input;
 
     const newProduct = {
@@ -27,12 +27,12 @@ exports.Mutation = {
       categoryId,
     };
 
-    products.push(newProduct);
+    db.products.push(newProduct);
 
     return newProduct;
   },
 
-  addReview: (parent, { input }, { reviews }) => {
+  addReview: (parent, { input }, { db }) => {
     const { date, title, comment, rating, productId } = input;
 
     const newReview = {
@@ -44,8 +44,32 @@ exports.Mutation = {
       productId,
     };
 
-    reviews.push(newReview);
+    db.reviews.push(newReview);
 
     return newReview;
+  },
+
+  deleteCategory: (parent, { id }, { db }) => {
+    db.categories = db.categories.filter((category) => category.id !== id);
+    db.products = db.products.map((product) => {
+      if (product.categoryId === id)
+        return {
+          ...product,
+          categoryId: null,
+        };
+      else return product;
+    });
+    return true;
+  },
+
+  deleteProduct: (parent, { id }, { db }) => {
+    db.products = db.products.filter((product) => product.id !== id);
+    db.reviews = db.reviews.filter((review) => review.productId !== id);
+    return true;
+  },
+
+  deleteReview: (parent, { id }, { db }) => {
+    db.reviews = db.reviews.filter((review) => review.id !== id);
+    return true;
   },
 };
